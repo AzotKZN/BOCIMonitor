523c74de4fb62e090a9fb40350507f534dee37e1
Azat Minvaliev <Miatozoff@gmail.com>
2015-03-02 21:51:00 +0200

    Initial Commit

---
+++ BackOfficeCIMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackOfficeCIMonitor
+//
+//  Created by Азат on 02.03.15.
+//  Copyright (c) 2015 Azat Minvaliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackOfficeCIMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackOfficeCIMonitor
+//
+//  Created by Азат on 02.03.15.
+//  Copyright (c) 2015 Azat Minvaliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
